4f4168e64b3f28dd59cb55b3adc63cf6fddfddc0
Abdurrohim Saifi <umum.saifi@gmail.com>
2019-12-01 18:27:27 +0700

    Preparing menu for admin & template default changes

--- resources/views/layouts/template-default.blade.php
+++ resources/views/layouts/template-default.blade.php
@@ -1,16 +1,4 @@
 <!DOCTYPE html>
-<!--
-Template Name: Frest HTML Admin Template
-Author: :Pixinvent
-Website: http:/www.pixinvent.com/
-Contact: hello@pixinvent.com
-Follow: www.twitter.com/pixinvents
-Like: www.facebook.com/pixinvents
-Purchase: https:/1.envato.market/pixinvent_portfolio
-Renew Support: https:/1.envato.market/pixinvent_portfolio
-License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
-
--->
 <html class="loading" lang="en" data-textdirection="ltr">
 <!-- BEGIN: Head-->
 
--- resources/views/layouts/template-default/menu.blade.php
+++ resources/views/layouts/template-default/menu.blade.php
@@ -17,20 +17,146 @@
                     <span class="menu-title" data-i18n="">Dashboard</span>
                 </a>
             </li>
-            <li class=" nav-item"><a href="#"><i class="menu-livicon" data-icon="morph-menu-arrow-bottom"></i><span class="menu-title" data-i18n="Menu Levels">Menu Levels</span></a>
+
+            <li class="navigation-header">
+                <span>Menu</span>
+            </li>
+
+            <li class=" nav-item">
+                <a href="#">
+                    <i class="menu-livicon" data-icon="unlock"></i>
+                    <span class="menu-title" data-i18n="">Verifikasi</span>
+                </a>
+            </li>
+
+            <li class=" nav-item">
+                <a href="#">
+                    <i class="menu-livicon" data-icon="grid"></i>
+                    <span class="menu-title" data-i18n="">Konsultasi</span>
+                </a>
+            </li>
+
+            <li class="navigation-header">
+                <span>Pengguna</span>
+            </li>
+
+            <li class=" nav-item">
+                <a href="#">
+                    <i class="menu-livicon" data-icon="users"></i>
+                    <span class="menu-title" data-i18n="">Dosen</span>
+                </a>
+            </li>
+
+            <li class=" nav-item">
+                <a href="#">
+                    <i class="menu-livicon" data-icon="users"></i>
+                    <span class="menu-title" data-i18n="">Mahasiswa</span>
+                </a>
+            </li>
+
+            <li class=" nav-item">
+                <a href="#">
+                    <i class="menu-livicon" data-icon="users"></i>
+                    <span class="menu-title" data-i18n="">Administrator</span>
+                </a>
+            </li>
+
+            <li class="navigation-header">
+                <span>Referensi Data</span>
+            </li>
+
+            <li class=" nav-item">
+                <a href="#">
+                    <i class="menu-livicon" data-icon="morph-map"></i>
+                    <span class="menu-title" data-i18n="">Wilayah</span>
+                </a>
                 <ul class="menu-content">
-                    <li><a href="#"><i class="bx bx-right-arrow-alt"></i><span class="menu-item" data-i18n="Second Level">Second Level</span></a>
+                    <li>
+                        <a href="#"><i class="bx bx-right-arrow-alt"></i>
+                            <span class="menu-item" data-i18n="">Provinsi</span>
+                        </a>
                     </li>
-                    <li><a href="#"><i class="bx bx-right-arrow-alt"></i><span class="menu-item" data-i18n="Second Level">Second Level</span></a>
+                    <li>
+                        <a href="#">
+                            <i class="bx bx-right-arrow-alt"></i>
+                            <span class="menu-item" data-i18n="">Kabupaten/Kota</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="bx bx-right-arrow-alt"></i>
+                            <span class="menu-item" data-i18n="">Kecamatan</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="bx bx-right-arrow-alt"></i>
+                            <span class="menu-item" data-i18n="">Kelurahan</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class=" nav-item">
+                <a href="#">
+                    <i class="menu-livicon" data-icon="building"></i>
+                    <span class="menu-title" data-i18n="">Fakultas</span>
+                </a>
+            </li>
+
+            <li class=" nav-item">
+                <a href="#">
+                    <i class="menu-livicon" data-icon="briefcase"></i>
+                    <span class="menu-title" data-i18n="">Jurusan</span>
+                </a>
+            </li>
+
+            <li class="navigation-header">
+                <span>Lainnya</span>
+            </li>
+
+            <li class=" nav-item">
+                <a href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                              document.getElementById('logout-form').submit();">
+                    <i class="menu-livicon" data-icon="close"></i>
+                    <span class="menu-title" data-i18n="">Keluar</span>
+                </a>
+            </li>
+
+            {{-- <li class=" nav-item">
+                <a href="#">
+                    <i class="menu-livicon" data-icon="morph-menu-arrow-bottom"></i>
+                    <span class="menu-title" data-i18n="">Menu Levels</span>
+                </a>
+                <ul class="menu-content">
+                    <li>
+                        <a href="#">
+                            <i class="bx bx-right-arrow-alt"></i>
+                            <span class="menu-item" data-i18n="">Second Level</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="bx bx-right-arrow-alt"></i>
+                            <span class="menu-item" data-i18n="">Second Level</span>
+                        </a>
                         <ul class="menu-content">
-                            <li><a href="#"><i class="bx bx-right-arrow-alt"></i><span class="menu-item" data-i18n="Third Level">Third Level</span></a>
+                            <li>
+                                <a href="#"><i class="bx bx-right-arrow-alt"></i>
+                                    <span class="menu-item" data-i18n="">Third Level</span>
+                                </a>
                             </li>
-                            <li><a href="#"><i class="bx bx-right-arrow-alt"></i><span class="menu-item" data-i18n="Third Level">Third Level</span></a>
+                            <li>
+                                <a href="#">
+                                    <i class="bx bx-right-arrow-alt"></i>
+                                    <span class="menu-item" data-i18n="">Third Level</span>
+                                </a>
                             </li>
                         </ul>
                     </li>
                 </ul>
-            </li>
+            </li> --}}
         </ul>
     </div>
 </div>
