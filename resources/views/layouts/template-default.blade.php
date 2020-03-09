03bae09512fe5ca2f3643ec4ffd545e9554327ce
Abdurrohim Saifi <abdurrohim.saifi@avocacode.com>
2020-03-09 16:59:08 +0700

    Fix “Mixed content blocked” when running an HTTP AJAX operation in an HTTPS page Ask

--- resources/views/layouts/template-default.blade.php
+++ resources/views/layouts/template-default.blade.php
@@ -3,6 +3,7 @@
 <!-- BEGIN: Head-->
 
 <head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
